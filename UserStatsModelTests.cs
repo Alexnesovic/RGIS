122b8bd7325b91a9a495987f81bd0b98de56531c
Alexnesovic <alex.nesovic@student.um.si>
2026-01-12 13:57:52 +0100

    Add files via upload

---
+++ UserStatsModelTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[TestClass]
+public class UserStatsModelTests
+{
+    [TestMethod]
+    public void Test_OnGet_SetsUsername()
+    {
+        // Arrange
+        var model = new UserStatsModel();
+
+        // Act
+        model.OnGet();
+
+        // Assert
+        Assert.AreEqual("testUser", model.Username);
+    }
+
+    [TestMethod]
+    public void Test_OnGet_SetsLoginCount()
+    {
+        // Arrange
+        var model = new UserStatsModel();
+
+        // Act
+        model.OnGet();
+
+        // Assert
+        Assert.AreEqual(14, model.LoginCount);
+    }
+
+    [TestMethod]
+    public void Test_OnGet_SetsCompletedTasks()
+    {
+        // Arrange
+        var model = new UserStatsModel();
+
+        // Act
+        model.OnGet();
+
+        // Assert
+        Assert.AreEqual(7, model.CompletedTasks);
+    }
+
+}
